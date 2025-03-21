fdfe51a0616f352ea9b5c7eead85ed00d59b6b45
akinmyde <akinremiolumide96@gmail.com>
2025-03-21 19:04:55 +0100

    chore(helper-setup): setup helpers package

--- apps/casinoB/app/page.tsx
+++ apps/casinoB/app/page.tsx
@@ -1,5 +1,6 @@
 import { BRAND_CONFIGS } from "@repo/config/brands"
 import { API_ROUTES } from "@repo/constants/api"
+import { isValidEmail } from "@repo/helpers/validation"
 
 export default function Page() {
   const casino = "CasinoA"
@@ -8,6 +9,7 @@ export default function Page() {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
         <div>{config.name}</div>
         <div>{API_ROUTES.GAMES}</div>
+        <div>{`${isValidEmail('akinremiolumide96@gmail.com')}`}</div>
     </main>
   );
 }
